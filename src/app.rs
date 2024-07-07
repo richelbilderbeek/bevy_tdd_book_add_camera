23803fcb3599b04a0f8bcb529f1ef90fa928064a
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-07 13:48:13 +0200

    EOD

--- src/app.rs
+++ src/app.rs
@@ -57,7 +57,7 @@ fn add_player_with_sprite_at_pos_with_scale(
 #[cfg(test)]
 fn count_n_players(app: &App) -> usize {
     let mut n = 0;
-    for c in app.world.components().iter() {
+    for c in app.world().components().iter() {
         // The complete name will be '[crate_name]::Player'
         if c.name().contains("Player") {
             n += 1;
@@ -68,39 +68,28 @@ fn count_n_players(app: &App) -> usize {
 
 #[cfg(test)]
 fn get_camera_scale(app: &mut App) -> f32 {
-    let mut query = app.world.query::<&OrthographicProjection>();
-    let projection = query.single(&app.world);
+    let mut query = app.world().query::<&OrthographicProjection>();
+    let projection = query.single(&app.world());
     projection.scale
 }
 
 #[cfg(test)]
 fn get_player_coordinat(app: &mut App) -> Vec3 {
-    let mut query = app.world.query::<(&Transform, &Player)>();
-    let (transform, _) = query.single(&app.world);
+    let mut query = app.world().query::<(&Transform, &Player)>();
+    let (transform, _) = query.single(&app.world());
     transform.translation
 }
 
 #[cfg(test)]
 fn get_player_scale(app: &mut App) -> Vec3 {
-    let mut query = app.world.query::<(&Transform, &Player)>();
-    let (transform, _) = query.single(&app.world);
+    let mut query = app.world().query::<(&Transform, &Player)>();
+    let (transform, _) = query.single(&app.world());
     transform.scale
 }
 
 #[cfg(test)]
-fn get_all_components_names(app: &App) -> Vec<String> {
-    use std::str::FromStr;
-
-    let mut v: Vec<String> = Vec::new();
-    for c in app.world.components().iter() {
-        v.push(String::from_str(c.name()).unwrap());
-    }
-    v
-}
-
-#[cfg(test)]
 fn has_camera(app: &App) -> bool {
-    for c in app.world.components().iter() {
+    for c in app.world().components().iter() {
         if c.name() == "bevy_render::camera::camera::Camera" {
             return true;
         }
@@ -109,20 +98,8 @@ fn has_camera(app: &App) -> bool {
 }
 
 #[cfg(test)]
-fn print_all_components_names(app: &App) {
-    for c in app.world.components().iter() {
-        println!("{}", c.name())
-    }
-}
-
-#[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_testing() {
-        assert_eq!(1 + 1, 2)
-    }
 
     #[test]
     fn test_can_create_app() {
@@ -207,70 +184,5 @@ mod tests {
         let mut app = create_app(params);
         app.update();
         assert_eq!(get_camera_scale(&mut app), custom_camera_scale);
-    }
-
-    #[test]
-    fn test_get_all_components_names_for_empty_app() {
-        let mut app = App::new();
-        app.update();
-        let v = get_all_components_names(&app);
-        assert_eq!(v.len(), 7);
-    }
-
-    #[test]
-    fn test_get_all_components_names_for_our_app() {
-        let mut app = create_app(create_default_game_parameters());
-        app.update();
-        let v = get_all_components_names(&app);
-        assert_eq!(v.len(), 24);
-    }
-
-    #[test]
-    fn test_print_names_of_empty_app() {
-        let mut app = App::new();
-        app.update();
-        print_all_components_names(&app);
-        /*
-        bevy_ecs::schedule::schedule::Schedules
-        bevy_ecs::reflect::AppTypeRegistry
-        bevy_app::main_schedule::MainScheduleOrder
-        bevy_app::main_schedule::FixedMainScheduleOrder
-        bevy_ecs::event::Events<bevy_app::app::AppExit>
-        */
-    }
-
-    #[test]
-    fn test_print_names_of_app_with_player() {
-        let mut app = App::new();
-        app.add_systems(Startup, add_player);
-        app.update();
-        print_all_components_names(&app);
-        /*
-        // First 5 of empty App, then
-        bevy_ecs::schedule::stepping::Stepping
-        bevy_tdd_book_add_player_sprite::app::Player
-        bevy_ecs::event::EventUpdateSignal
-        */
-    }
-
-    #[test]
-    fn test_print_names_of_app_with_player_sprite() {
-        let mut app = App::new();
-        app.add_systems(Startup, add_player_with_sprite);
-        app.update();
-        print_all_components_names(&app);
-        /*
-        // First 5 of empty App, then:
-        bevy_ecs::schedule::stepping::Stepping // From Player
-        bevy_sprite::sprite::Sprite
-        bevy_transform::components::transform::Transform
-        bevy_transform::components::global_transform::GlobalTransform
-        bevy_asset::handle::Handle<bevy_render::texture::image::Image>
-        bevy_render::view::visibility::Visibility
-        bevy_render::view::visibility::InheritedVisibility
-        bevy_render::view::visibility::ViewVisibility
-        bevy_tdd_book_add_player_sprite::app::Player // From Player
-        bevy_ecs::event::EventUpdateSignal // From Player
-        */
     }
 }
